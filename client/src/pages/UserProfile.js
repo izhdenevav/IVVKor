0d19d2d41b2d1b1ba1f6a0da7b998ab8a23cc0d0
79139 <vizdeneva@gmail.com>
2023-02-19 14:56:53 +0700

    18.02.2023

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -3,6 +3,14 @@ import {Context} from "../index";
 import {useNavigate} from "react-router-dom";
 import styles from "../css-modules/profile.module.css"
 
+const toNormalDate = (date) => {
+    let year = date.substr(0, 4)
+    let month = date.substr(5, 2)
+    let day = date.substr(8, 2)
+
+    return day + "." + month + "." + year
+}
+
 const UserProfile = () => {
     const {user} = useContext(Context)
 
@@ -17,14 +25,20 @@ const UserProfile = () => {
 
 
     return (
-        <div>
-            <div className={ styles.divPhoto }>
-                <img className={ styles.userPhoto } src={process.env.REACT_APP_API_URL + user._user.photo}></img>
+        <form className={ styles.profile }>
+            <div className={ styles.navBar }>
+                <button className={ styles.homeButton } type="button" onClick={signOut}>Выйти</button>
             </div>
-            <h1>{user._user.login}</h1>
-            <h1>{user._user.dateBirth}</h1>
-            <button type="button" onClick={signOut}>Выйти</button>
-        </div>
+            <div className={ styles.divUserInfo }>
+                <div className={ styles.divPhoto }>
+                    <img className={ styles.userPhoto } src={process.env.REACT_APP_API_URL + user._user.photo}></img>
+                </div>
+                <h1 className={ styles.text }>{user._user.login}</h1>
+                <h1 className={ styles.text }>{toNormalDate(user._user.dateBirth)}</h1>
+                <button className={ styles.redButton } >ред.</button>
+
+            </div>
+        </form>
     );
 };
 
